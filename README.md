21924ab0f6d083c193d0e63ff0cb97462e6e7044
Tran Tuan <tran.van.tuan@mqsolutions.com.vn>
2020-04-09 19:33:34 +0700

    update package

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ This package support extend function of Router in ExpressJS to grouping routes.
 
 ## Installation:
 ```
-npm install express-group-router
+npm i --save express-group-router
 ```
 
 ## Usage:
 ```js
 const express = require('express');
 const app = express();
-require('express-group-router')(express, app);
+require('express-group-router')(app);
 const fooMiddleware = (req, res, next) => {
     console.log('foo');
     next();
@@ -26,25 +26,35 @@ const barMiddleware = (req, res, next) => {
 }
 let router = express.Router();
 router.group({ prefix: '/api', middlewares: [fooMiddleware]}, (router) => {
-    
-    router.get('/hello-world', (req, res) => { // route: /api/hello-world, middlewares: fooMiddleware
+    // route: /api/hello-world, middlewares: fooMiddleware
+    router.get('/hello-world', (req, res) => { 
         res.send('Hello world');
     })
 
     router.group({prefix: '/test', middleware: barMiddleware }, (router) => {
-        router.get('/group', (req, res) => { // route: /api/test/group; Middlewares: fooMiddleware, barMiddleware;
+        // route: /api/test/group; Middlewares: fooMiddleware, barMiddleware;
+        router.get('/group', (req, res) => { 
             res.send('Test group');
         })
     })
 
     router.group([barMiddleware], (router) => { // not add sub prefix
-        router.get('/not-prefix', (req, res) => { // route: /api/not-prefix
+        // route: /api/not-prefix
+        router.get('/not-prefix', (req, res) => { 
             res.send('Not prefix');
         });
     })
 
-    router.group('/foo', (router) => { // not add middleware more...
-        router.get('/bar', (req, res) => { // route: /api/foo/bar
+    router.group('/prefix' ,[barMiddleware], (router) => { // not add sub prefix
+        // route: /api/prefix/alert
+        router.get('/alert', (req, res) => { 
+            res.send('Not prefix');
+        });
+    })
+
+    router.group('/foo', (router) => { // not add middleware more..
+        // route: /api/foo/bar
+        router.get('/bar', (req, res) => { 
             // Middlewares: fooMiddleware
             res.send('Foo bar')
         })
--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-module.exports = (express, app) => {
-    express.Router.group = function (...params) {
+'use strict';
+const express = require('express');
+
+module.exports = (app) => {
+    express.Router.group = function(...params) {
         let prefix;
         let middlewares = []
         let configure;
-
-        if (arguments.length === 2) {
-        if (typeof arguments[0] === 'object') {
-            prefix = arguments[0].prefix || '';
-            middlewares = arguments[0].middlewares || [];
-            if (typeof middlewares === 'function') {
-                middlewares = [middlewares]
+        if(arguments.length === 3) {
+            if(typeof arguments[0] === 'string') {
+                prefix = arguments[0];
+            } else if(typeof arguments[0] === 'function') {
+                middlewares = [arguments[0]]
+            } else if(Array.isArray(arguments[0])) {
+                middlewares = arguments[0]
             }
-        } else if (typeof arguments[0] === 'string') {
-            prefix = arguments[0];
-        } else if (typeof arguments[0] === 'function') {
-            middlewares = [arguments[0]];
-        } else if (Array.isArray(arguments[0])) {
-            middlewares = arguments[0];
-        }
-        configure = arguments[1];
-        } else if (arguments.length === 1 && typeof arguments[0] === 'function') {
+            if(typeof arguments[1] === 'function') {
+                middlewares = [arguments[1]]
+            } else if(typeof arguments[1] === 'string') {
+                prefix = arguments[1]
+            }
+            if(typeof arguments[2] === 'function') {
+                configure = arguments[2]
+            }
+        } else if(arguments.length === 2) {
+            if(typeof arguments[0] === 'object') {
+                prefix = arguments[0].prefix || '';
+                middlewares = arguments[0].middlewares || [];
+                if (typeof middlewares === 'function') {
+                    middlewares = [middlewares]
+                }
+            } else if(typeof arguments[0] === 'string') {
+                prefix = arguments[0];
+            } else if(typeof arguments[0] === 'function') {
+                middlewares = [arguments[0]];
+            } else if(Array.isArray(arguments[0])) {
+                middlewares = arguments[0];
+            }
+            configure = arguments[1];
+        } else if(arguments.length === 1 && typeof arguments[0] === 'function') {
             prefix = '';
             middlewares = [];
             configure = arguments[0]
         }
         let router = express.Router();
 
-        if (Array.isArray(middlewares) && middlewares.length > 0) {
+        if(Array.isArray(middlewares) && middlewares.length > 0) {
             router.use(...middlewares);
         }
-        if (prefix && prefix !== '' && prefix !== '/') {
-        if (this._prefix && this._prefix !== '/' && this._prefix !== '') {
-            app.use(this._prefix + prefix, router);
-            router._prefix = this._prefix + prefix;
+        if(prefix && prefix !== '' && prefix !== '/') {
+            if(this._prefix && this._prefix !== '/' && this._prefix !== '') {
+                app.use(this._prefix + prefix, router);
+                router._prefix = this._prefix + prefix;
+            } else {
+                router._prefix = prefix;
+                app.use(prefix, router);
+            }
         } else {
-            router._prefix = prefix;
-            app.use(prefix, router);
-        }
-        } else {
-            if (this._prefix && this._prefix !== '' && this._prefix !== '/') {
+            if(this._prefix && this._prefix !== '' && this._prefix !== '/') {
                 app.use(this._prefix, router);
                 router._prefix = this._prefix;
             } else {
@@ -46,7 +64,7 @@ module.exports = (express, app) => {
                 router._prefix = '';
             }
         }
-        if (configure && typeof configure === 'function') {
+        if(configure && typeof configure === 'function') {
             configure(router);
         }
         return router;
